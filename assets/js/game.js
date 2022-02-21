7c5995404b82e198832dd0362404edb81c730a98
dsotilloi <dsotilloi@outlook.com>
2022-02-21 18:58:47 -0300

    Creación de proyecto, assets y estilos.

---
+++ assets/js/game.js
@@ -0,0 +1,26 @@
+// 2C = 2 of clubs
+// 2D = 2 of diamonds
+// 2H = 2 of hearts
+// 2S = 2 of spades
+
+let deck = [];
+const types = ["C", "D", "H", "S"];
+const specials = ["A", "J", "Q", "K"];
+
+const createDeck = () => {
+  for (let i = 2; i <= 10; i++) {
+    for (let type of types) {
+      deck.push(i + type);
+    }
+  }
+
+  for(let type of types){
+    for(let special of specials){
+      deck.push(special + type);
+    }
+  }
+
+  console.log(deck);
+};
+
+createDeck();
