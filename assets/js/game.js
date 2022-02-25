0095d3adb90ee65557e1560c23e14d274b8bae61
dsotilloi <dsotilloi@outlook.com>
2022-02-24 21:02:43 -0300

    Creación de lógica y eventos para el botón "give me a deck"

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,6 +2,14 @@ let decks = [];
 const types = ["C", "D", "H", "S"];
 const specials = ["A", "J", "Q", "K"];
 
+let playerScore = 0,
+  ComputerScore = 0;
+
+//Referencias del HTML:
+const btnAsk = document.querySelector("#btnAsk");
+const playerDecks = document.querySelector("#playerDecks");
+let htmlScore = document.querySelectorAll("small");
+
 //Crea una nueva baraja:
 const createDecks = () => {
   for (let i = 2; i <= 10; i++) {
@@ -36,4 +44,31 @@ const deckValue = (deck) => {
   return isNaN(value) ? (value === "A" ? 11 : 10) : value * 1;
 };
 
-deckValue(askDeck());
+//Eventos:
+btnAsk.addEventListener("click", () => {
+  const deck = askDeck();
+
+  playerScore = playerScore + deckValue(deck);
+  htmlScore[0].innerText = playerScore;
+
+  const deckImg = document.createElement("img");
+  deckImg.src = `./assets/decks/${deck}.png`;
+  deckImg.classList.add("deck");
+  playerDecks.append(deckImg);
+
+  if (playerScore > 21) {
+
+    btnAsk.disabled = true;
+    setTimeout(() => {
+      alert("Game over 👾. Sumaste más de 21 puntos, ¡inténtalo de nuevo!");
+    }, 300);
+
+  }else if ( playerScore === 21) {
+
+    btnAsk.disabled = true;
+    setTimeout(() => {
+      alert("Ganaste 🏆 ¡Sumaste 21 puntos!");
+    }, 300);
+
+  }
+});
