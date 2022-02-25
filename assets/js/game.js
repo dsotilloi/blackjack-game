3b3474d5073818f99d142b2f9c92eeb80c29b927
dsotilloi <dsotilloi@outlook.com>
2022-02-25 15:57:42 -0300

    Desarrollo de lógica para detener juego y comenzar uno nuevo

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,15 +3,20 @@ const types = ["C", "D", "H", "S"];
 const specials = ["A", "J", "Q", "K"];
 
 let playerScore = 0,
-  ComputerScore = 0;
+  computerScore = 0;
 
 //Referencias del HTML:
 const btnAsk = document.querySelector("#btnAsk");
+const btnStop = document.querySelector("#btnStop");
+const btnNew = document.querySelector("#btnNew");
 const playerDecks = document.querySelector("#playerDecks");
+const computerDecks = document.querySelector("#computerDecks");
 let htmlScore = document.querySelectorAll("small");
 
 //Crea una nueva baraja:
 const createDecks = () => {
+  btnNew.disabled = true;
+
   for (let i = 2; i <= 10; i++) {
     for (let type of types) {
       decks.push(i + type);
@@ -44,6 +49,23 @@ const deckValue = (deck) => {
   return isNaN(value) ? (value === "A" ? 11 : 10) : value * 1;
 };
 
+//Comienza el turno de juego de la computadora:
+const computerTurn = (playerScore) => {
+  do {
+    const deck = askDeck();
+
+    computerScore = computerScore + deckValue(deck);
+    htmlScore[1].innerText = computerScore;
+
+    const deckImg = document.createElement("img");
+    deckImg.src = `./assets/decks/${deck}.png`;
+    deckImg.classList.add("deck");
+    computerDecks.append(deckImg);
+
+    if (playerScore > 21) break;
+  } while (computerScore < playerScore && playerScore < 21);
+};
+
 //Eventos:
 btnAsk.addEventListener("click", () => {
   const deck = askDeck();
@@ -57,18 +79,56 @@ btnAsk.addEventListener("click", () => {
   playerDecks.append(deckImg);
 
   if (playerScore > 21) {
-
+    btnNew.disabled = false;
     btnAsk.disabled = true;
+    btnStop.disabled = true;
+    computerTurn(playerScore);
+
     setTimeout(() => {
-      alert("Game over 👾. Sumaste más de 21 puntos, ¡inténtalo de nuevo!");
-    }, 300);
-
-  }else if ( playerScore === 21) {
-
+      alert("Game over 👾. Sumaste más de 21 puntos, la computadora gana.");
+    }, 1000);
+  } else if (playerScore === 21) {
+    btnNew.disabled = false;
     btnAsk.disabled = true;
-    setTimeout(() => {
-      alert("Ganaste 🏆 ¡Sumaste 21 puntos!");
-    }, 300);
+    btnStop.disabled = true;
+    computerTurn(playerScore);
 
+    setTimeout(() => {
+      alert("Ganaste 🏆 ¡Sumaste 21 puntos exactos!");
+    }, 1000);
   }
+});
+
+btnStop.addEventListener("click", () => {
+  btnNew.disabled = false;
+  btnAsk.disabled = true;
+  btnStop.disabled = true;
+  computerTurn(playerScore);
+
+  setTimeout(() => {
+    if (computerScore === playerScore) {
+      alert("Empate 🤝 ¡Buena jugada!");
+    } else if (computerScore > 21) {
+      alert("Ganaste 🏆 ¡Juega de nuevo!");
+    } else {
+      alert("Game over 👾. La computadora gana, ¡inténtalo de nuevo!");
+    }
+  }, 1000);
+});
+
+btnNew.addEventListener("click", () => {
+  decks = [];
+  decks = createDecks();
+
+  btnAsk.disabled = false;
+  btnStop.disabled = false;
+
+  playerScore = 0;
+  computerScore = 0;
+
+  htmlScore[0].innerText = 0;
+  htmlScore[1].innerText = 0;
+
+  computerDecks.innerHTML = "";
+  playerDecks.innerHTML = "";
 });
